a371a0c569c31702621a363969d33d0857fdf2f6
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-11-05 22:19:29 +0100

    VotingMachine draft

--- src/main/java/VotingMachine.java
+++ src/main/java/VotingMachine.java
@@ -1,7 +1,9 @@
 import java.time.Year;
+import java.util.*;
 
 public class VotingMachine {
     Year year;
+    List<Vote> votes = new ArrayList<>();
 
     public VotingMachine(Year year) {
         if (year == null) {
@@ -10,9 +12,69 @@ public class VotingMachine {
         this.year = year;
     }
 
-    public VotingMachine(){
+    public VotingMachine() {
         this.year = Year.now();
     }
 
+    public void add(Vote vote) {
+        if (vote == null) {
+            throw new NullPointerException();
+        }
+        // TODO
+    }
 
+    public void addAll(Collection<Vote> votes) {
+        if (votes == null) {
+            throw new NullPointerException();
+        }
+        // TODO
+    }
+
+    public List<Vote> getVotes() {
+        List<Vote> result = new ArrayList<>();
+
+        for (Vote vote : votes) {
+            result.add(vote.getClone());
+        }
+
+        return result;
+    }
+
+    public List<Voter> getVoters() {
+        List<Voter> result = new ArrayList<>();
+
+        for (Vote vote : votes) {
+            result.add(vote.getVoter().getClone());
+        }
+
+        return result;
+    }
+
+    public Vote getVote(Voter voter) {
+        if (voter == null) {
+            throw new NullPointerException();
+        }
+
+        for (Vote vote : votes) {
+            if (vote.getVoter().equals(voter)) {
+                return vote;
+            }
+        }
+
+        return null;
+    }
+
+    public Year getYear() {
+        return year;
+    }
+
+    public Map<Party, Double> getDistribution() {
+        // TODO
+        return null;
+    }
+
+    public List<String> showDistribution() {
+        // TODO
+        return null;
+    }
 }
