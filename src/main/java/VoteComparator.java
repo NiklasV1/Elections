639541c67268cfbf51df5568e001a51e117b4fe8
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-11-05 18:52:25 +0100

    formatting

--- src/main/java/VoteComparator.java
+++ src/main/java/VoteComparator.java
@@ -4,7 +4,7 @@ public class VoteComparator implements Comparator<Vote> {
 
     @Override
     public int compare(Vote o1, Vote o2) {
-        return switch (o1.getDateTime().compareTo(o2.getDateTime())){
+        return switch (o1.getDateTime().compareTo(o2.getDateTime())) {
             case -1 -> -1;
             case 1 -> 1;
             default -> switch (o1.getVoter().compareTo(o2.getVoter())) {
