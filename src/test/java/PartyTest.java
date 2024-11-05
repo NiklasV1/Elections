7c43fa8a54316bd7e0bce6b670b51abdcbe16157
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-11-05 16:26:11 +0100

    Party Test colorCodes

--- src/test/java/PartyTest.java
+++ src/test/java/PartyTest.java
@@ -7,5 +7,9 @@ class PartyTest {
     @Test
     void testColorCodes() {
         assertEquals(0xff0000, Party.REPUBLICAN.getColorCode());
+        assertEquals(0x0000ff, Party.DEMOCRAT.getColorCode());
+        assertEquals(0x008000, Party.GREEN.getColorCode());
+        assertEquals(0xffffff, Party.CONSTITUTION.getColorCode());
+        assertEquals(0xffd700, Party.LIBERTARIAN.getColorCode());
     }
 }
