f0a18c838e8dbfaab597258740a318d613ad93c2
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-11-05 18:20:27 +0100

    VoteComparator added

---
+++ src/main/java/VoteComparator.java
@@ -0,0 +1,23 @@
+package jpp.exam.elections;
+
+import java.util.Comparator;
+
+public class VoteComparator implements Comparator<Vote> {
+
+    @Override
+    public int compare(Vote o1, Vote o2) {
+        return switch (o1.getDateTime().compareTo(o2.getDateTime())){
+            case -1 -> -1;
+            case 1 -> 1;
+            default -> switch (o1.getVoter().compareTo(o2.getVoter())) {
+                case -1 -> -1;
+                case 1 -> 1;
+                default -> switch (o1.getParty().compareTo(o2.getParty())) {
+                    case -1 -> -1;
+                    case 1 -> 1;
+                    default -> 0;
+                };
+            };
+        };
+    }
+}
