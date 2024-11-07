54b109caf545f1bb7d852f341b86dd790608599b
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-11-07 16:41:11 +0100

    VotingConsole Test file

---
+++ src/test/java/VotingConsoleTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VotingConsoleTest {
+    @Test
+    void mainMenu() {
+
+    }
+
+    @Test
+    void createMenu() {
+    }
+
+    @Test
+    void distributionMenu() {
+    }
+
+    @Test
+    void allVotesMenu() {
+    }
+
+    @Test
+    void manipulateMenu() {
+    }
+}
